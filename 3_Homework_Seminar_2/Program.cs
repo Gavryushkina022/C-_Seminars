ff394c51dc43fbbcf1a8d4fe5d877cf19780172e
Gavryushkina022 <YANKOVAINNA@gmail.com>
2022-11-19 23:52:05 +0300

    3 Задача второго семинара

---
+++ 3_Homework_Seminar_2/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number <= 0 || number > 7) 
+{
+    Console.WriteLine("Введите число от 1 до 7");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+    if (number >= 6)
+    {
+        Console.WriteLine("День является выходным");
+    }
+    else
+    {
+        Console.WriteLine("День Не является выходным");
+    }
