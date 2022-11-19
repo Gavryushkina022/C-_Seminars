ba2018781411ceb1dd656fad0d2b89200f62381c
Gavryushkina022 <YANKOVAINNA@gmail.com>
2022-11-19 22:11:46 +0300

    Задача 1_Семинар 2

---
+++ 2_Homework_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка от 100 до 1000 --> {number}");
+
+int MinusMiddle(int num)
+{
+    int firstDigit = num % 100;
+    int secondDigit = firstDigit / 10; 
+    return secondDigit;
+}
+MinusMiddle(number);
+Console.WriteLine(MinusMiddle(number));
