06bf824eda0c70c15d741f40e333f6d2bd55393f
Gavryushkina022 <YANKOVAINNA@gmail.com>
2022-11-28 18:14:01 +0300

    Task_36_Homework_5

---
+++ Task_34_Homework_5/Program.cs
@@ -0,0 +1,41 @@
+﻿// // Задача 34: Задайте массив 
+// заполненный случайными 
+// положительными 
+// трёхзначными числами. Н
+// апишите программу, которая покажет количество чётных чисел в массиве.
+// // [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int GetNumEvenElem(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    { if (i % 2 == 0) sum++; }
+    return sum;
+}
+
+int[] arr = CreateArrayRndInt(4, 100, 1000);
+PrintArray(arr);
+int numEven = GetNumEvenElem(arr);
+Console.WriteLine($"Количество четных чисел = {numEven}");
